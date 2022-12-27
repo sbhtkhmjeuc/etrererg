4ea8b866184b65887eeb70bd54975c3ab2ade28d
sbhtkhmjeuc <sbhtkhmjeuc@gmail.com>
2022-12-27 14:28:28 -0800

    Second

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,30 @@
-var message = "Hello World";
-console.log(message);
+
+var calcString;
+
+function set () {
+    
+    document.getElementById("inputBox").value = "Blop";
+
+}
+
+function updatedString(id){
+
+    if (id != "undefined"){
+        calcString = calcString + id;
+        settInputText(calcString);
+    }
+    
+
+}
+
+function settInputText(value){
+    document.getElementById("inputBox").value = value;
+}
+ 
+function resetting(){
+
+    calcString = '';
+    settInputText('')
+
+
+}
